554fd506a629589f9009c6b915611d1022515c90
Horváth Dávid <horvath@webarticum.hu>
2017-12-03 11:31:10 +0100

    Change tab policy to spaces

--- j2php/src/main/java/hu/webarticum/j2php/Main.java
+++ j2php/src/main/java/hu/webarticum/j2php/Main.java
@@ -7,12 +7,13 @@ import com.github.javaparser.ast.CompilationUnit;
 
 public class Main {
 
-	public static void main(String[] args) {
-		InputStream inputStream = Main.class.getResourceAsStream("/sample/SampleClass.java.src");
-		
-		CompilationUnit compilationUnit = JavaParser.parse(inputStream);
-		
-		System.out.println(new VerySimplePhpTranslator(compilationUnit));
-	}
+    public static void main(String[] args) {
+        InputStream inputStream = Main.class.getResourceAsStream("/sample/SampleClass.java.src");
+        
+        CompilationUnit compilationUnit = JavaParser.parse(inputStream);
+        
+        System.out.println(new VerySimplePhpTranslator(compilationUnit));
+    }
 
 }
+
