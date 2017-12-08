fa837949278ebbdbd32aa50fe08cf459c32ad30b
horvath <horvath@patronet.net>
2017-12-08 16:36:44 +0100

    Improve ClassTranslator

--- j2php/src/main/java/hu/webarticum/j2php/ClassTranslator.java
+++ j2php/src/main/java/hu/webarticum/j2php/ClassTranslator.java
@@ -1,8 +1,18 @@
 package hu.webarticum.j2php;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.InitializerDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
 public class ClassTranslator {
@@ -55,10 +65,73 @@ public class ClassTranslator {
         }
 
         outputBuilder.append(" {\n");
-        
+
+        EmbeddingContext innerEmbeddingContext = embeddingContext.moreIndent("    ");
+        outputBuilder.append(innerEmbeddingContext.indent + "\n");
+
         {
-            EmbeddingContext innerEmbeddingContext = embeddingContext.moreIndent("    ");
-            outputBuilder.append(innerEmbeddingContext.indent + "\n");
+        	// TODO / XXX
+        	
+            List<FieldDeclaration> fields = classDeclaration.getFields();
+            for (FieldDeclaration field: fields) {
+                String visibilityName = "public";
+                EnumSet<Modifier> modifiers = field.getModifiers();
+                if (modifiers.contains(Modifier.PRIVATE)) {
+                    visibilityName = "private";
+                } else if (modifiers.contains(Modifier.PROTECTED)) {
+                    visibilityName = "protected";
+                }
+                boolean isStatic = modifiers.contains(Modifier.STATIC);
+                NodeList<VariableDeclarator> variables = field.getVariables();
+                int variableCount = variables.size();
+                String[] variableNames = new String[variables.size()];
+                for (int i = 0; i < variableCount; i++) {
+                    variableNames[i] = variables.get(i).getNameAsString();
+                }
+                outputBuilder.append("    " + visibilityName + (isStatic ? " static" : "") + " $" + String.join(", $", variableNames) + ";\n\n");
+            }
+            if (!fields.isEmpty()) {
+            	outputBuilder.append("\n");
+            }
+        }
+        
+        // XXX
+        List<String> staticInitializerNames = new ArrayList<String>();
+        
+        List<String> instanceInitializerNames = new ArrayList<String>();
+        {
+            for (BodyDeclaration<?> member: classDeclaration.getMembers()) {
+                if (member.isInitializerDeclaration()) {
+                    InitializerDeclaration initializer = (InitializerDeclaration)member;
+                    if (initializer.isStatic()) {
+                        String postFix = staticInitializerNames.isEmpty() ? "" : "" + (staticInitializerNames.size() + 1);
+                        String associatedName = "_staticInit" + postFix;
+                        staticInitializerNames.add(associatedName);
+                        outputBuilder.append("    public static " + associatedName + "() {\n        // TODO\n    }\n\n");
+                    } else {
+                        String postFix = instanceInitializerNames.isEmpty() ? "" : "" + (instanceInitializerNames.size() + 1);
+                        String associatedName = "_init" + postFix;
+                        instanceInitializerNames.add(associatedName);
+                        outputBuilder.append("    private " + associatedName + "() {\n        // TODO\n    }\n\n");
+                    }
+                }
+            }
+        }
+
+        {
+        	// TODO
+        	
+            List<ConstructorDeclaration> constructors = classDeclaration.getConstructors();
+            for (ConstructorDeclaration constructor: constructors) {
+                int parameterCount = constructor.getParameters().size();
+                outputBuilder.append("    // TODO: constructor with " + parameterCount + " parameter(s)\n\n");
+            }
+            if (!constructors.isEmpty()) {
+            	outputBuilder.append("\n");
+            }
+        }
+
+        {
             {
                 
                 // TODO: grouping by name
@@ -73,46 +146,7 @@ public class ClassTranslator {
         outputBuilder.append(embeddingContext.indent + "}");
         
         
-        
-        
-        
-        
-        
-        /*
-        
-        {
-            List<String> instanceInitializerNames = new ArrayList<String>();
-            {
-                for (BodyDeclaration<?> member: clazz.getMembers()) {
-                    if (member.isInitializerDeclaration()) {
-                        InitializerDeclaration initializer = (InitializerDeclaration)member;
-                        if (initializer.isStatic()) {
-                            String postFix = staticInitializerNames.isEmpty() ? "" : "" + (staticInitializerNames.size() + 1);
-                            String associatedName = "_staticInit" + postFix;
-                            staticInitializerNames.add(associatedName);
-                            resultBuilder.append("    public static " + associatedName + "() {\n        // TODO\n    }\n\n");
-                        } else {
-                            String postFix = instanceInitializerNames.isEmpty() ? "" : "" + (instanceInitializerNames.size() + 1);
-                            String associatedName = "_init" + postFix;
-                            instanceInitializerNames.add(associatedName);
-                            resultBuilder.append("    private " + associatedName + "() {\n        // TODO\n    }\n\n");
-                        }
-                    }
-                }
-            }
-            
-            {
-                List<ConstructorDeclaration> constructors = clazz.getConstructors();
-                for (ConstructorDeclaration constructor: constructors) {
-                    int parameterCount = constructor.getParameters().size();
-                    resultBuilder.append("    // TODO: constructor with " + parameterCount + " parameter(s)\n\n");
-                }
-                if (!constructors.isEmpty()) {
-                    resultBuilder.append("\n");
-                }
-            }
-        }
-            
+        /* TODO / FIXME
         for (String staticInitializedName: staticInitializerNames) {
             resultBuilder.append("\n" + className + "::" + staticInitializedName + "();\n");
         }
